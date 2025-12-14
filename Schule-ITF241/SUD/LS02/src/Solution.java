5e0c9e31c45835d4b3039ae24b7170881741dafc
Elshad-92 <elshadgaucho1992@gmail.com>
2025-12-14 15:39:27 +0100

    Add Solution.java to LS02 as example

---
+++ Schule-ITF241/SUD/LS02/src/Solution.java
@@ -0,0 +1,9 @@
+// LS02: Beispiel-Klasse 'Solution' für deine Aufgabe
+// Füge hier deinen Lösungs-Code ein.
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Solution for LS02");
+    }
+}
+
