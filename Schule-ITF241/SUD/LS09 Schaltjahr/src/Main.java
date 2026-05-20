ff418153b2ebd0c4ae9c8e68b55c114b5f8ee5cd
Elshad Gulaliyev <gulaels12@k2ac.intern>
2026-05-20 12:39:55 +0200

    aufgabe schaltjahr

---
+++ Schule-ITF241/SUD/LS09 Schaltjahr/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Datum d = new Datum(15, 02, 2012);
+
+        d.anzeigeFormat();
+
+        System.out.println(d.alsTage());
+    }
+}
